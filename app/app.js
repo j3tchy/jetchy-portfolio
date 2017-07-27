5a77d830006c83d7fafb16fb53c3aba44c1bfb8b
Jermaine Gyekye <JGyekye@brightsource.co.uk>
2017-07-27 11:19:32 +0000

    extract text plugin - external css

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+import styles from './assets/scss/style.scss';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ var webpack = require('webpack');
 var path = require('path');
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = {
     entry: './app/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -15,16 +17,23 @@ var config = {
                 test: /\.(js)$/,
                 exclude: '/node_modules/',
                 use: 'babel-loader'
+            },{
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
             }
         ]
     },
-    devServer: {
-        contentBase: './dist'
-    },
     plugins: [
+        new ExtractTextPlugin('css/style.css'),
         new HtmlWebpackPlugin({
             title: 'Development',
-            template: './app/index.html'
+            template: 'app/index.html'
         })
     ]
 }
