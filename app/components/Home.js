9d7a5d4653b00ffed419744bd31b307076ad94c4
Jermaine Gyekye <JGyekye@brightsource.co.uk>
2017-07-29 23:38:02 +0000

    components

---
+++ app/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+function Home() {
+    return (
+        <div id="home" className="section container">
+            <div className="container__wrapper">
+                <header className="container__header">
+                    <h1>Title</h1>
+                    <h2>Small Heading</h2>
+                </header>
+                <div className="container__panel">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ app/components/Skills.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+function Skills() {
+    return (
+        <div className="section container" id="skills">
+            <div className="container__panel">
+                <h2>Skills</h2>
+                <ul>
+                    <li>Test</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
