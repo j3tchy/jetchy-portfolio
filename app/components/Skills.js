ecef34f37405df96c38f51d1a34f4c1a3a7d9f59
Jermaine Gyekye <jgyekye@googlemail.com>
2017-07-30 00:33:11 +0000

    install tools

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -3,12 +3,7 @@ import React, { Component } from 'react';
 function Skills() {
     return (
         <div className="section container" id="skills">
-            <div className="container__panel">
-                <h2>Skills</h2>
-                <ul>
-                    <li>Test</li>
-                </ul>
-            </div>
+
         </div>
     )
 }
